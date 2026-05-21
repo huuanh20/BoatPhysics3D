e16cd1c9da9937705f7c4ab7b52b4c1f551aa0e2
huuanh20 <165425332+huuanh20@users.noreply.github.com>
2026-05-21 11:18:26 +0700

    refactor: restructure project by moving frontend to frontend/ folder

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, send_from_directory, request
 from flask_socketio import SocketIO, emit
 
-app = Flask(__name__, static_folder='.', static_url_path='')
+app = Flask(__name__, static_folder='frontend', static_url_path='')
 app.config['SECRET_KEY'] = 'vietnam_to_france_boat_racing_secret!'
 socketio = SocketIO(app, cors_allowed_origins="*")
 
